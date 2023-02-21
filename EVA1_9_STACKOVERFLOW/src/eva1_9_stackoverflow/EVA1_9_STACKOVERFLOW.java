25ebf9b1e0eb435c3fd0b0408369e9153e7b76db
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-02-21 17:18:34 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_stackoverflow;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_9_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO co
+        A();
+    }
+    public static void A(){
+        A ();
+    }
+    
+}
