c89410c73aa877d94b2bd89c0da82cd720833552
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-02-21 17:17:43 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_7_METODOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       myMethod (); //invocar al metodo:  llamada a funcion
+    }
+    public static void myMethod(){
+        System.out.println("you have called me: my name is myMethod");
+        
+    }
+}
