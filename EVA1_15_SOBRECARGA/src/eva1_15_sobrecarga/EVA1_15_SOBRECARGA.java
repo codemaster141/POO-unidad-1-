853948f3e99ba0e94e3639c05a07d22f40e2f0be
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-02-26 12:14:04 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**darle mas funcones de las que tenia a un metodo-----------------------------------------------------------------------
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //diferntes println: son 10 diferentes metodos, para imprimir diferentes tipos de datos
+        //para crear sobre carga de metodos la firma debe ser unica
+        System.out.println("hola undo");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        
+    }
+    
+}
