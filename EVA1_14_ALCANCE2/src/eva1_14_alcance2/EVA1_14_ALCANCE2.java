acf7007ddf8745e12419f0947766776ab223d279
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-02-26 12:13:36 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE2/src/eva1_14_alcance2/EVA1_14_ALCANCE2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_14_alcance2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_14_ALCANCE2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;//visible dentro de todo el bloque main();
+        for (int i = 0; i < 10; i++) {
+            int x = 100;//la variable esta declarada en main() y
+                             //es visible dentro del for
+                             int y = 100;//visible dentro del for
+            if(1 == 5){
+                
+            }
+        }
+        int y = 100;//aqui la variadle y del ciclo for no existen
+        //podemos declarr una variable del mismo 
+    }
+    
+}
